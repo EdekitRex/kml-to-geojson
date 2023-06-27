53649c4d0fe4bc84207818746f27cf96b591a058
Brent Ramsey <brent.ramsey@diggsly.com>
2023-06-27 07:27:28 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,114 @@
 const xml2js = require('xml2js');
 const { promisify } = require('util');
-import { GeoJsonObject } from 'geojson';
+import { GeoJsonObject, GeoJsonProperties, Feature, Point, LineString, Polygon } from 'geojson';
 
-const parseString = promisify(xml2js.parseString);
-
-function parseCoordinateString(coordinates: any) {
-    const points = coordinates.split(' ').map((point: { split: (arg0: string) => [any, any, any]; }) => {
-        const [longitude, latitude, altitude] = point.split(',');
-        return [parseFloat(longitude), parseFloat(latitude), parseFloat(altitude)];
-    });
-
-    return points.length === 1 ? points[0] : points;
+export interface KmlIconStyleProps {
+    'icon-color'?: string;
+    'icon-opacity'?: number;
+    'icon-size'?: number;
+    'icon-image'?: string;
+    'text-color'?: string;
+    'text-opacity'?: number;
+    'text-size'?: number;
 }
 
-function parseStyle(style: { IconStyle: any; }) {
-    const iconStyle = style.IconStyle;
-    const icon = iconStyle && iconStyle.Icon;
-    const color = iconStyle && iconStyle.color;
-    const href = icon && icon.href;
-
-    return {
-        color: color || null,
-        href: href || null,
-    };
+export interface KmlLineStyleProps {
+    'line-color'?: string;
+    'line-opacity'?: number;
+    'line-width'?: number;
 }
 
-export default async function convertKMLtoGeoJSON(kmlString: any) {
-    const kml = await parseString(kmlString, { explicitArray: false });
-    const placemarks = kml.kml.Document.Placemark;
+export interface KmlPolyStyleProps {
+    'fill-color'?: string;
+    'fill-opacity'?: number;
+    'fill-outline-color'?: string;
+}
 
-    const features = placemarks.map((placemark: { name: any; description: any; ExtendedData: any; Point: any; Style: any; }) => {
-        const { name, description, ExtendedData, Point, Style } = placemark;
-        const { coordinates } = Point;
+export type KmlStyleProps = KmlIconStyleProps | KmlLineStyleProps | KmlPolyStyleProps;
 
-        const properties = {
-            style: {
-                color: null,
-                href: null,
-            }
-        };
-        if (ExtendedData && ExtendedData.Data) {
-            ExtendedData.Data.forEach((data: { displayName: any; value: any; }) => {
-                const key = data.displayName;
-                const value = data.value;
-                // @ts-ignore
-                properties[key] = value;
-            });
-        }
+export type KmlPointFeature<P = GeoJsonProperties> = Omit<Feature<Point, (P & KmlIconStyleProps)>, "bbox">;
 
-        const geometry = {
-            type: 'Point',
-            coordinates: parseCoordinateString(coordinates),
-        };
+export type KmlLineFeature<P = GeoJsonProperties> = Omit<Feature<LineString, (P & KmlLineStyleProps)>, "bbox">;
 
-        const style = parseStyle(Style);
-        properties.style = style;
+export type KmlPolyFeature<P = GeoJsonProperties> = Omit<Feature<Polygon, (P & KmlPolyStyleProps)>, "bbox">;
+
+export type KmlFeature<P = GeoJsonProperties> = KmlPointFeature<P> | KmlLineFeature<P> | KmlPolyFeature<P>;
+
+export interface KmlGeojson<P = GeoJsonProperties> {
+    type: 'FeatureCollection';
+    features: Array<KmlFeature<P>>;
+}
+
+export class convertKMLtoGeoJSON {
+    private parseString = promisify(xml2js.parseString);
+
+    private readonly parseCoordinateString = (coordinates: any) => {
+        const points = coordinates.split(' ').map((point: { split: (arg0: string) => [any, any, any]; }) => {
+            const [longitude, latitude, altitude] = point.split(',');
+            return [parseFloat(longitude), parseFloat(latitude), parseFloat(altitude)];
+        });
+
+        return points.length === 1 ? points[0] : points;
+    }
+
+    private readonly parseStyle = (style: any) => {
+        const iconStyle = style.IconStyle;
+        const icon = iconStyle && iconStyle.Icon;
+        const color = iconStyle && iconStyle.color;
+        const href = icon && icon.href;
 
         return {
-            type: 'Feature',
-            properties,
-            geometry,
+            color: color || null,
+            href: href || null,
         };
-    });
+    }
 
-    const geoJSON = {
-        type: 'FeatureCollection',
-        features,
-    };
+    public readonly parse = async (kmlString: string): Promise<
+        GeoJsonObject
+    > => {
+        const kml = await this.parseString(kmlString, {explicitArray: false});
+        const placemarks = kml.kml.Document.Placemark;
 
-    return geoJSON;
+        const features = placemarks.map( (placemark: any ) => {
+            const {name, description, ExtendedData, Point, Style} = placemark;
+            const {coordinates} = Point;
+
+            const properties = {
+                style: {
+                    color: null,
+                    href: null,
+                }
+            };
+            if (ExtendedData && ExtendedData.Data) {
+                ExtendedData.Data.forEach((data: { displayName: any; value: any; }) => {
+                    const key = data.displayName;
+                    const value = data.value;
+                    // @ts-ignore
+                    properties[key] = value;
+                });
+            }
+
+            const geometry = {
+                type: 'Point',
+                coordinates: this.parseCoordinateString(coordinates),
+            };
+
+            const style = this.parseStyle(Style);
+            properties.style = style;
+
+            return {
+                type: 'Feature',
+                properties,
+                geometry,
+            };
+        });
+
+        const geoJSON: KmlGeojson = {
+            type: 'FeatureCollection',
+            features,
+        };
+
+        return geoJSON;
+
+    }
 }
